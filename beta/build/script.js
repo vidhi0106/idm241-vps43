6d9e26b01ce450c5d43114ccea634281d9db790b
vidhi0106 <119354699+vidhi0106@users.noreply.github.com>
2023-11-05 14:18:06 -0500

    beta build

---
+++ beta/build/script.js
@@ -0,0 +1,59 @@
+let timeout;
+let modalTimeout;
+
+document.getElementById('likeButton').addEventListener('mouseenter', function() {
+    clearTimeout(timeout);
+    document.getElementById('myModal').style.display = 'flex';
+    const images = document.querySelectorAll('.modal-images img');
+    images.forEach((image, index) => {
+        setTimeout(() => {
+            image.classList.add('show');
+        }, index * 100);
+    });
+
+    images.forEach((image) => {
+        image.addEventListener('mouseenter', function() {
+            clearTimeout(modalTimeout); // Clear previous timeout
+            images.forEach((img) => {
+                if (img !== image) {
+                    img.style.transition = 'transform 0.3s ease';
+                    img.style.transform = 'scale(0.8)';
+                }
+            });
+            image.style.transition = 'transform 0.3s ease';
+            image.style.transform = 'scale(1.2)';
+        });
+
+        image.addEventListener('mouseleave', function() {
+            images.forEach((img) => {
+                img.style.transition = 'transform 0.3s ease';
+                img.style.transform = 'scale(1)';
+            });
+        });
+    });
+});
+
+document.getElementById('myModal').addEventListener('mouseleave', function() {
+    const modal = document.getElementById('myModal');
+    modal.style.transition = 'opacity 0.2s ease';
+    modal.style.opacity = '0';
+
+    modalTimeout = setTimeout(() => {
+        modal.style.display = 'none';
+        modal.style.opacity = '1'; // Reset opacity for next interaction
+    }, 300); // Adjust the duration of the fade out effect if needed
+});
+
+document.getElementById('likeButton').addEventListener('mouseleave', function() {
+    document.getElementById('myModal').style.display = 'flex'; // Keep the modal visible
+    clearTimeout(timeout);
+    modalTimeout = setTimeout(() => {
+        document.getElementById('myModal').style.display = 'none';
+        const images = document.querySelectorAll('.modal-images img');
+        images.forEach((image) => {
+            image.classList.remove('show');
+        });
+    }, 650); // Set to 650ms for the modal to stay after hover off
+});
+
+
